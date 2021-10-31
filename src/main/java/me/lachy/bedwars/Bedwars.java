a3aedeb71c8bb1c214f99087215377ca4c13fcec
lqchy <lachlantheslayer@gmail.com>
2021-10-31 17:36:32 +0000

    Testing

--- src/main/java/me/lachy/bedwars/Bedwars.java
+++ src/main/java/me/lachy/bedwars/Bedwars.java
@@ -7,7 +7,7 @@ public final class Bedwars extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getLogger().info("Hello");
     }
 
     @Override
